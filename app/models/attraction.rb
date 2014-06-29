07c6e2f38510b64795324580e9d35d88f5fc2f55
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-06-29 11:38:48 +0100

    basic geo coder added

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,3 +1,6 @@
 class Attraction < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
+  belongs_to :user
 end
