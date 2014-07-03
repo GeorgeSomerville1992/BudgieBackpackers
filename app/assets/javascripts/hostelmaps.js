6487816a268a40f6c2847ae39be7bf2115bbc7c2
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-03 11:12:59 +0100

    some changes with google maps

--- app/assets/javascripts/hostelmaps.js
+++ app/assets/javascripts/hostelmaps.js
@@ -3,32 +3,51 @@ $(function(){
     hostels = gon.hostels.HotelListResponse.HotelList.HotelSummary
     hostellatitude = hostels.latitude
     hostellongitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.longitude
+    hostelId = hostels.hotelId
     // for(i=0; i<20; i++){
-    $.each(hostels, function(i, hostel){
+      // this will go in a window from google
+    $.each(hostels, function(i, hostel){ // hidden form?  put this inside one then user submit - will this data be able to show???
     searchresult = '<li>' + 'hotel name' + hostel.name + ' '  + 'hostellowrate' + ' ' + 
     ' ' + hostel.lowRate + ' ' + 'hostelhighrate' + ' ' + hostel.highRate + 'hostel.lattude' +
-    + hostel.latitude + '</li>' + $("input").attr("type", "button").click(function(){console.log('clicked')})
+    + hostel.latitude + '</li>' + $("input").attr("type", "button").click(function(){
+                                  console.log('clicked')
+                                  // some ajax call - to shift data back into rails
+                                  $.ajax({
+                                    type:post,
+                                    url:"/hostels",
+                                    //data: hostelId
+                                    dataType:json // or script???
 
-    $('#hostelResults').append(searchresult)
+      })
     })
 
+    $('#hostelResults').append(searchresult)
+    }) // then how do get it in rails params? 
+      // or i could put the whole thing in rails - but that wont work with google maps?
+      // we just need the list actually so maybe it will
+      // string intreplate in rails
+      // but then displaying data inside the markers as well as the list to save may look at bit shit???
 
-var fetchlatitude = $('#latitude').data('hostels') // data is the instance variable!!!
-var fetchlongitude = $('#longitude').data('hostels')
+var fetchLatitude = $('#latitude').data('hostels') // data is the instance variable!!!
+var fetchLongitude = $('#longitude').data('hostels')
 var map;
 var infowindow;
+var hotelRed = new google.maps.MarkerImage('/assets/hotel_0star_red.png');
 
 hostels = gon.hostels.HotelListResponse.HotelList.HotelSummary
-hostellatitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.latitude
-hostellongitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.longitude
-
-
+hostelLatitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.latitude
+hostelLongitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.longitude
+hostelLowrate = gon.hostels.HotelListResponse.HotelList.HotelSummary.lowRate
+//
 function createMarkerForhostel(hostel){
     var latLng = new google.maps.LatLng(hostel.latitude,hostel.longitude);
     var marker = new google.maps.Marker({
+      // if hostel low rate - ....then display yello or red or green!!!
+      
       position: latLng,
       map: window.map,
-      title: "hi"
+      title: "hi",
+      icon: hotelRed
       // wtf is this???
       //camera_id: camera.id
     }) 
@@ -40,13 +59,13 @@ function maphostels(hostels){
   // _(attactions).each(createMarkerForAttraction)
   $.each(hostels,function(i,hostel){
         createMarkerForhostel(hostel)
-
-  })
-}
+        // add a specfic id to this marker
+  })    //iterate through this marker 
+}       // then pass this marker back to the server end instead of the hotel marker itself. 
 function initialize() { 
     var mapOptions = {
       zoom: 12,                     // hostel based on what user has typed in
-      center: new google.maps.LatLng(fetchlatitude, fetchlongitude),
+      center: new google.maps.LatLng(fetchLatitude, fetchLongitude),
       mapTypeId: google.maps.MapTypeId.SATELLITE
     }
       // do i need ajax for this??? 
