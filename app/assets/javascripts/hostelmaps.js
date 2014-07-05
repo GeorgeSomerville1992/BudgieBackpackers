cd888bdaa76d2e3507ccbdd7494204ccc7277f27
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-05 10:06:36 +0100

    fixed

--- app/assets/javascripts/hostelmaps.js
+++ app/assets/javascripts/hostelmaps.js
@@ -1,10 +1,7 @@
-$(function(){ 
-  var map;
-  var infowindow;
+$(function(){  
   if (gon.hostels){
     hostels = gon.hostels.HotelListResponse.HotelList.HotelSummary
     hostellatitude = hostels.latitude
-
     hostellongitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.longitude
      //var hostelId = gon.hostels.HotelListResponse.HotelList.HotelSummary[i].hotelId
     hostelLowrate = gon.hostels.HotelListResponse.HotelList.HotelSummary.lowRate
@@ -16,10 +13,8 @@ $(function(){
     $.each(hostels, function(i, hostel){ // hidden form?  put this inside one then user submit - will this data be able to show???
       
       $('#hostelResults').append(hostelTemplate(hostel))
+      
     })
-
-
-
 
     $('.hostel-info-button').on("click", function(hostelId){
       //console.log("clicked hostel info button with hotelId: " + $(this).data("hotelId"))
@@ -43,6 +38,9 @@ $(function(){
         $('#hostelDetailedResults').append(hostelDetailedTemplate(hostel))
       })
     }
+
+
+
     // searchresult = '<li>' + 'hotel name' + hostel.name + ' '  + 'hostellowrate' + ' ' + 
     // ' ' + hostel.lowRate + ' ' + 'hostelhighrate' + ' ' + hostel.highRate + 'hostel.lattude' +
     // + hostel.latitude + '</li>' + $("input").attr("type", "button").click(function(){
@@ -66,63 +64,53 @@ $(function(){
 
 var fetchLatitude = $('#latitude').data('hostels') // data is the instance variable!!!
 var fetchLongitude = $('#longitude').data('hostels')
-
+var map;
+var infowindow;
 var hotelRed = new google.maps.MarkerImage('/assets/hotel_0star_red.png');
 var hotelGreen = new google.maps.MarkerImage('/assets/hotel_0star_green.png')
 var hotelYellow = new google.maps.MarkerImage('/assets/hotel_0star_yellow.png')
 var hotelOrange = new google.maps.MarkerImage('/assets/hotel_0star_orange.png')
-
-// function addInfoWindowForHostel(marker, hostels){
-//     google.maps.event.addListener(marker, 'click', function(){
-//       console.log("value of infowindow", infowindow)
-//       if(infowindow != undefined) infowindow.close()
-//       infowindow = new google.maps.InfoWindow({
-//         content: '<p>' + hostel.name + '</p>'
-//       })
-
-      
-//       map.setCenter(new google.maps.LatLng((marker.position.lat()), marker.position.lng())); 
-//       map.setZoom(18);
-//       marker.infowindow.open(map, marker);
-//     })
-//   }
-
-
-  function createMarkerForhostel(hostel, lowrate){
-      var latLng = new google.maps.LatLng(hostel.latitude,hostel.longitude);
-      var lowRate = lowrate;
-      console.log(lowRate);
-      if(hostel.lowRate <= 40){
-        var marker = new google.maps.Marker({
-          position: latLng,
-          map: window.map,
-          title: "hi",
-          icon: hotelGreen
-        })
-      }else if(hostel.lowRate <= 50){
-        var marker = new google.maps.Marker({
+hostels = gon.hostels.HotelListResponse.HotelList.HotelSummary
+hostelLatitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.latitude
+hostelLongitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.longitude
+var hostelLowrate = gon.hostels.HotelListResponse.HotelList.HotelSummary.lowRate
+//
+function createMarkerForhostel(hostel, lowrate){
+    var latLng = new google.maps.LatLng(hostel.latitude,hostel.longitude);
+    var lowRate = lowrate;
+    console.log(lowRate);
+    if(hostel.lowRate <= 40){
+      var marker = new google.maps.Marker({
         position: latLng,
         map: window.map,
         title: "hi",
-        icon: hotelYellow
-        })
-      }else if(hostel.lowRate <= 70){
-        var marker = new google.maps.Marker({
-        position: latLng,
-        map: window.map,
-        title: "hi",
-        icon: hotelOrange
-        })
-       }else if(hostel.lowRate <= 90){
-        var marker = new google.maps.Marker({
-        position: latLng,
-        map: window.map,
-        title: "hi",
-        icon: hotelRed
-        })
-      }  
-     //addInfoWindowForHostel(marker, hostel)   
-    };  
+        icon: hotelGreen
+      })
+    }else if(hostel.lowRate <= 50){
+      var marker = new google.maps.Marker({
+      position: latLng,
+      map: window.map,
+      title: "hi",
+      icon: hotelYellow
+      })
+    }else if(hostel.lowRate <= 70){
+      var marker = new google.maps.Marker({
+      position: latLng,
+      map: window.map,
+      title: "hi",
+      icon: hotelOrange
+      })
+     }else if(hostel.lowRate <= 90){
+      var marker = new google.maps.Marker({
+      position: latLng,
+      map: window.map,
+      title: "hi",
+      icon: hotelRed
+      })
+    }     
+
+
+
     // var marker = new google.maps.Marker({
     //   // if hostel low rate - ....then display yello or red or green!!!
     //   if(hostelLowrate <= 50){
@@ -142,46 +130,18 @@ var hotelOrange = new google.maps.MarkerImage('/assets/hotel_0star_orange.png')
       //camera_id: camera.i
   
     // call mapoattractions
-    
-
+    //addInfoWindowForCamera(marker, camera)
+  }
   // what are we actualky passing into the attaction
   function maphostels(hostels){
-
     // _(attactions).each(createMarkerForAttraction)
     $.each(hostels,function(i,hostel){
-      //var hotel = gon.hostels.HotelListResponse.HotelList.HotelSummary[i]
-      var hotel = gon.hostels.HotelListResponse.HotelList.HotelSummary[i]
-      //hotelLowrate = hotel.lowRate
-      createMarkerForhostel(hostel, hostelLowrate)
-        
-        // if(hotel != undefined)
-          // var hostelLowrate = hotel.lowRate
-        // else
-          // var hostelLowrate = 0   
+
+        var hostelLowrate = gon.hostels.HotelListResponse.HotelList.HotelSummary[i].lowRate
+        createMarkerForhostel(hostel, hostelLowrate)
           // add a specfic id to this marker
     })    //iterate through this marker 
   }       // then pass this marker back to the server end instead of the hotel marker itself. 
-
-// function maphostels(hostels){
-//     // _(attactions).each(createMarkerForAttraction)
-//     $.each(hostels,function(i,hostel){
-
-//         var hostelLowrate = gon.hostels.HotelListResponse.HotelList.HotelSummary[i].lowRate
-//         createMarkerForhostel(hostel, hostelLowrate)
-//           // add a specfic id to this marker
-//     })    //iterate through this marker 
-//   }       // then pass this marker back to the server end instead of the hotel marker itself. 
-
-
-
-
-
-
-
-
-
-
-  
   function initialize() { 
       var mapOptions = {
         zoom: 12,                     // hostel based on what user has typed in
@@ -194,25 +154,15 @@ var hotelOrange = new google.maps.MarkerImage('/assets/hotel_0star_orange.png')
       //   dataType: "json",
       //   success: mapattractions
       // });
-      
-      map_container = document.getElementById('map-canvas')
-      if(map_container != undefined){
-        window.map = new google.maps.Map(map_container, mapOptions) 
-        maphostels(gon.hostels)
-        
-        //infowindow = new google.maps.InfoWindow(); 
-      }
-      // var contentString = '<p>' + hostels.name + '</p>';
-      // if(contentString != undefined){
-
-
-      // }
-      // put informaiton from api in here??? create a new function from this
-      // built in service near by - calling function and request
-      
+    window.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions) 
+    maphostels(hostels)
+    
+    infowindow = new google.maps.InfoWindow(); // put informaiton from api in here??? create a new function from this
+     // built in service near by - calling function and request
   }   
 
+
  google.maps.event.addDomListener(window, "load", initialize)
- });
+})
 
 
