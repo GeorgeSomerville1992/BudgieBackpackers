33a36d39be43aba3bb1c61561fb2650ac97d5895
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-06 17:29:56 +0100

    added bad home page, fixed devise problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          :omniauth_providers => [:google_oauth2]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name , :image
   # attr_accessible :title, :body
 
   has_many :hostels
