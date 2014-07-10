58a180b5245e8790fc1742e523840aee6a37c63f
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-10 10:38:52 +0100

    changes

--- app/assets/javascripts/googleearth.js
+++ app/assets/javascripts/googleearth.js
@@ -1,24 +1,24 @@
-// var ge;
-//     google.load("earth", "1", {"other_params":"sensor=true_or_false"});
+var ge;
+    google.load("earth", "1", {"other_params":"sensor=true_or_false"});
 
-//     function init() {
-//       google.earth.createInstance('map3d', initCB, failureCB);
-//     }
+    function init() {
+      google.earth.createInstance('map3d', initCB, failureCB);
+    }
 
-//     function initCB(instance) {
-//       ge = instance;
-//       ge.getWindow().setVisibility(true);
-//       // add a navigation control
-//       ge.getNavigationControl().setVisibility(ge.VISIBILITY_AUTO);
+    function initCB(instance) {
+      ge = instance;
+      ge.getWindow().setVisibility(true);
+      // add a navigation control
+      ge.getNavigationControl().setVisibility(ge.VISIBILITY_AUTO);
 
-//       // add some layers
-//       ge.getLayerRoot().enableLayerById(ge.LAYER_BORDERS, true);
-//       ge.getLayerRoot().enableLayerById(ge.LAYER_ROADS, true);
+      // add some layers
+      ge.getLayerRoot().enableLayerById(ge.LAYER_BORDERS, true);
+      ge.getLayerRoot().enableLayerById(ge.LAYER_ROADS, true);
 
       
-//         }
+        }
 
-//     function failureCB(errorCode) {
-//     }
+    function failureCB(errorCode) {
+    }
 
-//     google.setOnLoadCallback(init);
+    google.setOnLoadCallback(init);
