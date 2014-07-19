0470b36c2dacb0ffab4515a8988d311fb79c0c99
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-19 17:22:51 +0100

    change

--- app/assets/javascripts/mymansory.js
+++
@@ -1,9 +0,0 @@
-
-  var $containter = $('#container');
-      $containter.imagesLoaded( function(){
-          $containter.masonry({
-            itemSelector: '.box',
-            isAnimated: !Modernizr.csstransitions,
-            isFitWidth: true
-       });  
-  });
