08cc678487702d5d151c2473faaf4d0270956309
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-27 12:14:12 +0100

    layout changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :body, :date_created, :date_edited, :name, :picture, :title
+  attr_accessible :body, :date_created, :date_edited, :name, :picture, :title, :location
   mount_uploader :picture, AvatarUploader 
   belongs_to :user
   belongs_to :hostel
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140726222508) do
+ActiveRecord::Schema.define(:version => 20140727110550) do
 
   create_table "attractions", :force => true do |t|
     t.string   "address"
@@ -57,12 +57,7 @@ ActiveRecord::Schema.define(:version => 20140726222508) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.integer  "user_id"
-  end
-
-  create_table "roles", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "location"
   end
 
   create_table "users", :force => true do |t|
