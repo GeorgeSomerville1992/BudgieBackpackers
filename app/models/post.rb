9c13c80e7fcc4dd43e9ba4d0b8a1970e8b8f7b3f
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-27 10:07:39 +0100

    test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :date_created, :date_edited, :name, :picture, :title
-  mount_uploader :picture, AvatarUploader 
+  # mount_uploader :picture, AvatarUploader 
   belongs_to :user
   belongs_to :hostel
   has_many :comments
