4653223b909987a97e3bb18555d0dc93f8503eb2
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-27 09:16:46 +0100

    recreated heroku

--- db/schema.rb
+++ db/schema.rb
@@ -59,12 +59,6 @@ ActiveRecord::Schema.define(:version => 20140726222508) do
     t.integer  "user_id"
   end
 
-  create_table "roles", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
