41fc5e43e7c8358df28742543b3d4b0f518ca2d9
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-27 12:19:26 +0100

    fixed conflcits

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
   attr_accessible :body, :date_created, :date_edited, :name, :picture, :title, :location
-  mount_uploader :picture, AvatarUploader 
+  # mount_uploader :picture, AvatarUploader 
 
   belongs_to :user
   belongs_to :hostel
