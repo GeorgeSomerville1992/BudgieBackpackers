190cb2a92eb7698aa444e16a94f71be061f87bdb
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-30 10:05:53 +0100

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,7 @@
 # ['registered', 'banned', 'moderator', 'admin'].each do |role|
 #   Role.find_or_create_by_name({name: role})
 # end
+
+george = User.new(name: 'George', email: "george@ga.com", password: "password")
+james = User.new(name: 'James', email: "james@ga.com", password: "password")
+josh = User.new(name: 'Josh', email: "josh@ga.com", password: "password")
