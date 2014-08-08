5f23cf658d90abd2e8eae5ff992d50a1a67b0c59
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-08-08 12:11:49 +0100

    removed general bloat comments

--- app/assets/javascripts/hostelmaps.js
+++ app/assets/javascripts/hostelmaps.js
@@ -24,9 +24,6 @@ $(function(){
         if (typeof hostel.postalCode == "undefined") {
           hostel.postalCode = "not applicable"
         }
-
-
-
         $('#hostelResults').append(hostelTemplate(hostel,i))
 
         // how would you render erb into a string????? 
@@ -68,12 +65,7 @@ $(function(){
         }
         
         $('#hotelOfferResults').append(hostelOfferTemplate(hostel))
-
-        
-
       })
-
-
     }
 
     if (gon.hostel_attraction_foursquare){
@@ -82,23 +74,12 @@ $(function(){
       attractions_foursquare_items = attractions_foursquare[0].items 
       attractions_foursquare_template = _.template($('#attraction-foursquare-template').text())
       attractions_foursquare_venues = attractions_foursquare[0]['items']
-      
+
      console.log(attractions_foursquare_items)
       
       a = attractions_foursquare[0].items
       // debugger
-      $.each(attractions_foursquare_items, function(i, item){ // hidden form?  put this inside one then user submit - will this data be able to show???
-        // console.log(item.venue)
-        // if (typeof item.venue.price.currency == "undefined") {
-        //   item.venue.price.currency = "not applicable"
-        // }
-        // if (typeof item.price.message == "undefined") {
-        //   item.price.message = "not applicable"
-        // }
-        /// <img src="<%= tips[0].user.photo.prefix %>60x60<%= tips[0].user.photo.suffix %>" 
-        // :plain
-        //   <img src="<%= ['tips'][0].user.photo.prefix %>60x60<%= ['tips'][0].user.photo.suffix %>"
-        //console.log(item.tips['0'].user.photo.prefix )
+      $.each(attractions_foursquare_items, function(i, item){ 
         if (typeof item.venue.rating == "undefined") {
           item.venue.rating = "not applicable"
         }
@@ -118,10 +99,6 @@ $(function(){
         $('#showfoursquaredata').append(attractions_foursquare_template(item.venue))
         
       })
-
-      
-
-
       for(var i = 0; i < a.length; i ++) { 
         
         $('#showfoursquaredata').append(a[i].venue.name)
@@ -208,26 +185,7 @@ $(function(){
 
 
     }
-
-
-
-    //   for(var i = 0; i < a.length; i ++) { 
-
-    //     $('#test').append(a[i].venue.name)
-
-    //   }
-
-
-    // }
-   
-    // $('#hostelResults').append(searchresult)
-   // then how do get it in rails params? 
-      // or i could put the whole thing in rails - but that wont work with google maps?
-      // we just need the list actually so maybe it will
-      // string intreplate in rails
-      // but then displaying data inside the markers as well as the list to save may look at bit shit???
-
-var fetchLatitude = $('#latitude').data('hostels') // data is the instance variable!!!
+var fetchLatitude = $('#latitude').data('hostels') 
 var fetchLongitude = $('#longitude').data('hostels')
 var map;
 var map_container;
@@ -247,85 +205,7 @@ var restaurant = new google.maps.MarkerImage('/assets/restaurant.png')
 var coffee = new google.maps.MarkerImage('/assets/coffee.png')
 var general = new google.maps.MarkerImage('/assets/panoramicview.png')
 var beer = new google.maps.MarkerImage('/assets/beer.png')
-// var fetchHostelAttraction = $('#hotel_attraction').data('hostels')
-  
-//   $('.showyelp').click(function() {
-//     var url = "http://api.yelp.com/" +
-//             "business_review_search?"+
-//             "&term=" + fetchHostelAttraction + 
-//             "&lat=" + fetchLatitude +
-//             "&long=" + fetchLongitude +
-//             "&ywsid=" + "tEgG-fgQ2KUyHahOv7gWBQ"
-//     $.ajax({
-//       type: 'GET',
-//       url: url,
-//         dataType: 'json',
-//         success:function(){
-//             displayYelpURL();
-//       }
-//       })
-//   });
-//     function displayYelpURL() {
-//         //return encodeURI(url);
-//        console.log("done")
-//     }
-    
-  
 
-//hostels = gon.hostels.HotelListResponse.HotelList.HotelSummary
-//hostelLatitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.latitude
-//hostelLongitude = gon.hostels.HotelListResponse.HotelList.HotelSummary.longitude
-//var hostelLowrate = gon.hostels.HotelListResponse.HotelList.HotelSummary.lowRate
-//
-
-// function addInfoWindowForHostel(marker, hostel){
-//   google.maps.event.addListener(marker, 'click', function(){
-//     console.log("i have just been clicked")
-//     if(infowindow != undefined) infowindow.close()
-//       infowindow = new google.maps.InfoWindow({
-//         content: "<p> i have just been clicked! </p> "
-
-//     })
-//     map.setCenter(new google.maps.LatLng((marker.position.lat()), marker.position.lng()));
-//     map.setZoom(18);
-//     infowindow.open(map, marker);
-
-
-//   })
-// }  
-
-// function initializemapDragHostels(mapcenter){
-//   var mapOptions = {
-//       zoom: 12,                     // hostel based on what user has typed in
-//       center: mapcenter,
-//       mapTypeId: google.maps.MapTypeId.ROADMAP
-//   }
-//   map_container = document.getElementById('map-canvas')
-//     if(map_container != undefined){
-//       window.map = new google.maps.Map(map_container, mapOptions) 
-//       maphostels(hostels)
-//       // gon.hostels is was messing everything up
-//       //infowindow = new google.maps.InfoWindow(); 
-//   }
-
-// }
-
-
-// function calcRoute(marker,hostel) {
-//   var end = new google.maps.LatLng(hostel.latitude,hostel.longitude);
-//   var start = $("#address").data('hostels');
-//   console.log(end)
-//   var request = {
-//       origin:start,
-//       destination:end,
-//       travelMode: google.maps.TravelMode.DRIVING
-//   };
-//   directionsService.route(request, function(response, status) {
-//     if (status == google.maps.DirectionsStatus.OK) {
-//       directionsDisplay.setDirections(response);
-//     }
-//   });
-// }
 function directRoute(position) {
   var start = $("#address").data('hostels');
   console.log(start)
@@ -363,13 +243,6 @@ function createMarkerForhostel(hostel, lowrate){
                     '<p>' + 'Postcode:' + ' ' + hostel.postalCode + '</p>' +
                     '<img width=100px, height=100px, src= http://images.travelnow.com/'+ hostel.thumbNailUrl + '>' + '<br/>' +
                     '<a id="directions" class="button tiny">Calculate Directions</a>' + '</div>'
-    // var contentString =
-    //   '<h3>' + hostel.name + '</h3>' +
-    //   '<p>' + 'Price:' + ' ' + '<b>' + hostel.lowRate + '</b>' + '</p>' +
-    //   '<p>' + 'Address:' + ' ' + hostel.address1 + '</p>' +
-    //   '<p>' + 'Postcode:' + ' ' + hostel.postalCode + '</p>' +
-    //   '<img src= http://images.travelnow.com/'+ hostel.thumbNailUrl + '>' +
-    //   '<a id="directions" class="button tiny">Calculate Directions</a>'
     
     if(hostel.lowRate <= 40){
       var marker = new google.maps.Marker({
@@ -455,83 +328,7 @@ function createMarkerForhostel(hostel, lowrate){
     };     
   
 
-    //dragNewHostels(mapcenter)
-    // var marker = new google.maps.Marker({
-    //   // if hostel low rate - ....then display yello or red or green!!!
-    //   if(hostelLowrate <= 50){
-    //   position: latLng,
-    //   map: window.map,
-    //   title: "hi",
-    //   icon: hotelgreen
-    // } else if (hostelLowrate >= 70){
-    //   position: latLng,
-    //   map: window.map,
-    //   title: "hi",
-    //   icon: hotelRed
-
-    // }
-    
-
-    // google.maps.event.addListener(marker, 'click', function(){
-    //   console.log("hello")
-    //     var contentString = '<h3>' + hostel.name + '</h3>' +
-    //                 '<p>' + 'Price:' + ' ' + '<b>' + hostel.lowRate + '</b>' + '</p>' +
-    //                 '<p>' + 'Address:' + ' ' + hostel.address1 + '</p>' +
-    //                 '<p>' + 'Postcode:' + ' ' + hostel.postalCode + '</p>' +
-    //                 '<img src= http://images.travelnow.com/'+ hostel.thumbNailUrl + '>' +
-    //                 '<a id="directions" class="button tiny">Calculate Directions</a>'
-    //     var thisMarker = this;
-    //     console.log(thisMarker)
-    //     infowindow.setContent(contentString);
-    //     infowindow.open(window.map,this);
-    //     $('a#directions').on('click', function(ev){
-    //       ev.preventDefault();
-          
-    //         directRoute(thisMarker.position);
-          
-    //     })
-    //   if(infowindow != undefined) infowindow.close()
-    //   infowindow = new google.maps.InfoWindow({
-    //     content: "hello"
-    //   })
-    //   // map_container.setCenter(new google.maps.LatLng((marker.position.lat()), marker.position.lng())); 
-    //   // map_container.setZoom(18);
-     
-    //  })
-      // wtf is this???
-      //camera_id: camera.i
-
-    //calcRoute(marker,hostel)
-    // console.log(mapcenter.B)
-    // call mapoattractions
-    //addInfoWindowForHostel(marker, hostel)
-    
-    // console.log(mapcenter)
-    // google.maps.event.addListener(window.map, 'dragend',function() { 
-    // var newCoords = window.map.getCenter(mapcenter);
-    //   console.log(newCoords)
-
-    //   var mapOptions = {
-    //     zoom: 12,                     // hostel based on what user has typed in
-    //     center: new google.maps.LatLng(newCoords.k, newCoords.B),
-    //     mapTypeId: google.maps.MapTypeId.ROADMAP
-    //   }
-
-
-
-    //   map_container = document.getElementById('map-canvas')
-    //   if(map_container != undefined){
-    //     window.map = new google.maps.Map(map_container, mapOptions) 
-    //     var mapcenter = window.map.getCenter();
-    //     console.log(mapcenter)
-    //     maphostels(hostels,mapcenter)
-    //     // gon.hostels is was messing everything up
-    //     //infowindow = new google.maps.InfoWindow(); 
-    //   }
-
-    // // maphostels(hostel,mapcenter)
-      
-    // });
+   
 }
   function directRouteAttraction(position) {
   var start = $("#address").data('hostels');
@@ -563,15 +360,6 @@ function createMarkerForhostel(hostel, lowrate){
   function createMarkerForAttraction(attraction, attraction_catagory, locationLat, locationLong,attractionDetails){
     var attractionCatagory = attraction_catagory
     var latLng = new google.maps.LatLng(locationLat,locationLong);  
-    //console.log(attractionCatagory)
-    
-     // if(attractionCatagory == "Bar"){
-     //  var marker = new google.maps.Marker({
-     //    position: latLng,
-     //    map: window.mapAttraction,
-     //    title: "hi",
-     //    icon: bar
-     //  })
       if(attractionCatagory == "Bar"){
       var marker = new google.maps.Marker({
         position: latLng,
@@ -614,10 +402,6 @@ function createMarkerForhostel(hostel, lowrate){
       // '<p>' + attractionDetails.likes.summary + '</p>'+
       '<p>' + "Avarage Rating:"+ attractionDetails.rating +  '</p>' +
       '<a id="directionsattractions" class="button tiny">Calculate Directions</a>' + '</div>'
-
-      // '<p>' + hostel.address1 + '</p>' +
-      // '<p>' + hostel.postalCode + '</p>' +
-      // '<img src= http://images.travelnow.com/'+ hostel.thumbNailUrl + '>'
         var thisMarker = this;
 
         infowindow.setContent(contentStringAttraction);
@@ -627,98 +411,22 @@ function createMarkerForhostel(hostel, lowrate){
           
             directRouteAttraction(thisMarker.position);
           
-        })
-
-      // if(infowindow != undefined) infowindow.close()
-      // infowindow = new google.maps.InfoWindow({
-      //   content: "hello"
-      // })
-      // map_container.setCenter(new google.maps.LatLng((marker.position.lat()), marker.position.lng())); 
-      // map_container.setZoom(18);
-     
+        })     
     })
-
-
-    //   google.maps.event.addListener(window.mapAttraction, 'dragend',function() { 
-    //     var newCoordsAttraction = window.mapAttraction.getCenter();
-        
-
-    //     var mapOptions = {
-    //       zoom: 12,                     // hostel based on what user has typed in
-    //       center: new google.maps.LatLng(newCoordsAttraction.k, newCoordsAttraction.B),
-    //       mapTypeId: google.maps.MapTypeId.ROADMAP
-    //     }
-    //     map_container_attraction = document.getElementById('map-attraction-canvas')
-    //     if(map_container_attraction != undefined){
-    //     window.mapAttraction = new google.maps.Map(map_container_attraction, mapOptions) 
-    //     var clientId = "PJOVUMNXMNMSCGSYVETRKZ23WN2LUR31M0AD04AMKTJAKI5I"
-    //     var clientSecret = "3GG355R0B5D4KMH1J1UIUFXH2ZZCFH4ISOW5WTNYV11JJTDV"
-    //     var api_Version = '20120610'
-    //     var query = $("#hotel_attraction").data('hostels');
-    //     var requestUrl = 'https://api.foursquare.com/v2/venues/explore?ll='+ newCoordsAttraction.k+','+newCoordsAttraction.B +'&query='+ query + '&price=1&venuePhotos=true&client_id='+ clientId + '&client_secret='+ clientSecret+'&v='+ api_Version
-    //     //console.log(requestUrl)
-    //     //attractions_foursquare = 
-    //     //https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD
-    //     $.ajax({
-    //       url: requestUrl,
-    //       dataType: "json",
-    //       complete: showAttractionAjax
-    //     });
-    //     function showAttractionAjax(response){
-    //       raw_data = response.responseText
-          
-    //       var format = JSON.parse(raw_data);
-    //       var groups = format.response['groups']
-    //       //console.log(groups)
-    //       attractions_foursquare = groups
-    //       mapattractions(attractions_foursquare)
-
-    //     }
-    //     // gon.hostels is was messing everything up
-    //     //infowindow = new google.maps.InfoWindow(); 
-    //   }
-
-    // // maphostels(hostel,mapcenter)
-      
-    // });
-
-
-
-
   }
-
-
-
-
-  // what are we actualky passing into the attaction
   function maphostels(hostels){
-    // _(attactions).each(createMarkerForAttraction)
     $.each(hostels,function(i,hostel){
-      // console.log(mapcenter)
-        //var hostelLowrate = gon.hostels.HotelListResponse.HotelList.HotelSummary[i].lowRate
         createMarkerForhostel(hostel, hostelLowrate)
-          // add a specfic id to this marker
-    })    //iterate through this marker 
-  }       // then pass this marker back to the server end instead of the hotel marker itself. 
+    })     
+  }       
   function mapattractions(attractions_foursquare){
     
     $.each(attractions_foursquare[0]['items'], function(i,attraction){
-
-        // console.log(attraction);
-        // console.log(attraction['venue']);
-        // console.log(attraction['venue']['categories']);
-        // console.log(attraction['venue']['categories']['0']);
         var locationLat = attraction['venue']['location']['lat']
         var locationLong = attraction['venue']['location']['lng']
         var categoryName = attraction['venue']['categories']['0']['name']
         var attractionDetails = attraction['venue']
-        // venue location - lat
-        //attractions_foursquare_items[0]['venue']['categories'][0]['name']
-        // var attraction_catagory = attractions_foursquare_items.categories[i].name
-
-        //var attractionname = attractions.name
         createMarkerForAttraction(attraction, categoryName,locationLat, locationLong,attractionDetails)
-          // add a specfic id to this marker
     }) 
   }
   function initialize() { 
